5d64e3a6f9c5dbcfab27f6aeec5d938d7c938bd1
Luis Felipe Gaitan Zambrano <87459956+luisfegz@users.noreply.github.com>
2024-12-08 00:21:40 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import clientPromise from '@/lib/mongodb'
 
-const adminEmails = ['luisfelipegaitanzambrano@gmail.com'];
+const adminEmails = ['luisfelipegaitanzambrano@gmail.com','beastsupplycol@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
